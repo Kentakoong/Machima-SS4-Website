586ca71ce07b20c173a84758d8e510d57ad4a362
Wongkraiwich Chuenchomphu <wongkraiwich@inedible.dev>
2024-05-14 17:16:38 +0700

    edit nav

--- src/layouts/Navbar/Navbar.tsx
+++ src/layouts/Navbar/Navbar.tsx
@@ -1,24 +1,66 @@
-/* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
 import { MachimaLogo } from "@machima/components/Machima/Logo";
 import Link from "next/link";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [open, setOpen] = useState(false);
+
   return (
-    <nav className="px-8 py-2">
-      <div className="flex justify-between bg-white border drop-shadow-xl rounded-full px-4 py-2 w-full *:flex *:items-center">
-        <div className="gap-2">
-          <Link href="/">
-            <MachimaLogo className="size-12 hover:scale-105 transition ease-in-out hover:drop-shadow-sm bg-white rounded-full" />
-          </Link>
-        </div>
-        <div className="gap-4">
-          {navList.map((it, idx) => (
-            <Link key={idx} href={it.href}>
-              {it.title}
-            </Link>
-          ))}
-        </div>
-      </div>
+    <nav className="px-4 md:px-8 py-2">
+      <Menu>
+        {({ open }) => (
+          <div className="flex flex-col sm:flex-row justify-between bg-white border drop-shadow-xl rounded-3xl sm:rounded-full px-4 py-2 w-full *:flex *:items-center">
+            <div className="flex justify-between sm:justify-start gap-2">
+              <Link href="/">
+                <MachimaLogo className="size-12 hover:scale-105 transition ease-in-out hover:drop-shadow-sm bg-white rounded-full" />
+              </Link>
+              <MenuButton
+                className={`${open ? "rotate-90" : ""} sm:hidden transition`}
+                onClick={() => setOpen(!open)}
+              >
+                {open ? (
+                  <XMarkIcon className="size-6" />
+                ) : (
+                  <Bars3Icon className="size-6" />
+                )}
+              </MenuButton>
+            </div>
+            <div>
+              <MenuItems className="flex sm:hidden justify-center w-full text-center">
+                <div
+                  className={`${
+                    !open ? "hidden sm:flex" : "flex"
+                  } flex-col sm:flex-row sm:mt-0 w-full mt-2`}
+                >
+                  {navList.map((it, idx) => (
+                    <MenuItem key={idx}>
+                      <Link
+                        className="hover:bg-gray-100 w-full rounded-lg transition py-2"
+                        href={it.href}
+                      >
+                        {it.title}
+                      </Link>
+                    </MenuItem>
+                  ))}
+                </div>
+              </MenuItems>
+              <div
+                className={`hidden px-4 gap-4 sm:flex flex-col sm:flex-row sm:mt-0 w-full mt-2`}
+              >
+                {navList.map((it, idx) => (
+                  <MenuItem key={idx}>
+                    <Link href={it.href}>{it.title}</Link>
+                  </MenuItem>
+                ))}
+              </div>
+            </div>
+          </div>
+        )}
+      </Menu>
     </nav>
   );
 };
